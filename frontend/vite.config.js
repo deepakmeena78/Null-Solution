37428e45a01c8a85da423aee9d79e2bdd5e6627d
deepakmeena78 <deepakmeenaa78@gmail.com>
2025-07-18 16:51:55 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//     build: {
-//     outDir: 'dist'
-//   },
-//   base: './'
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+    build: {
+    outDir: 'dist'
   },
-  base: './',
-  server: {
-    historyApiFallback: true,
-  }
-});
+  base: './'
+})
